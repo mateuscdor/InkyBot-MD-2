07df73634138e6b1aec59a31ac77a0eb813e36a4
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-03-14 12:37:10 -0300

    Update inky.js

--- inky.js
+++ inky.js
@@ -2,14 +2,14 @@
     Libreria
 */
 const {
-  default: makeWASocket,
-  useSingleFileAuthState,
-  DisconnectReason,
-  generateForwardMessageContent,
-  prepareWAMessageMedia,
-  generateWAMessageFromContent,
-  generateMessageID,
-  downloadContentFromMessage
+    default: makeWASocket,
+    useSingleFileAuthState,
+    DisconnectReason,
+    generateForwardMessageContent,
+    prepareWAMessageMedia,
+    generateWAMessageFromContent,
+    generateMessageID,
+    downloadContentFromMessage
 } = require('@adiwajshing/baileys-md')
 const { state, saveState } = useSingleFileAuthState('./session.json')
 const fs = require('fs')
@@ -20,41 +20,54 @@ const pino = require('pino')
 */
 const { smsg } = require('./lib/functions')
 
+function nocache(module, cb = () => { }) {
+    fs.watchFile(require.resolve(module), async () => {
+        await uncache(require.resolve(module))
+        cb(module)
+    })
+}
+function uncache(module = '.') {
+	return new Promise((resolve, reject) => {
+		try {
+			delete require.cache[require.resolve(module)]
+			resolve()
+		} catch (e) {
+			reject(e)
+		}
+	})
+}
+
 const start = async() => {
-  const inky = makeWASocket({
-    logger: pino({ level: 'silent' }),
-    printQRInTerminal: true,
-    browser: ['InkyBot-MD','',''],
-    auth: state,
-    version: [2, 2204, 13]
-  })
-  
-  inky.ev.on('messages.upsert', (mek) => {
-    try {
-      const x = mek.messages[0]
-      if (!x.message) return
-      
-      x.message = (Object.keys(x.message)[0] === 'ephemeralMessage') ? x.message.ephemeralMessage.message : x.message
-      if (x.key && x.key.remoteJid === 'status@broadcast') return
-      
-      const m = smsg(inky, x)
-      require('./message/upsert')(inky, m, mek)
-    } catch (e) {
-      var isError = String(e)
-      console.log(isError)
-    }
-  })
-  
-  inky.ev.on('connection.update', async (update) => {
-    const { connection, lastDisconnect } = update
-    if (connection === 'close') {
-      lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? start() : console.log('Bot Desconectado')
-    }
-  })
-  
-  inky.ev.on('creds.update', saveState)
-  
-  return inky
+    const inky = makeWASocket({
+        logger: P({ level: 'silent' }),
+        printQRInTerminal: true,
+        auth: state
+    })
+    
+    require('./message/upsert.js')
+    nocache('./message/upsert.js', module => console.log('El archivo upsert.js ha sido actualizado'))
+    
+    inky.ev.on('messages.upsert', (mek) => {
+        const x = mek.messages[0]
+        if (!x.message) return
+        
+        x.message = (Object.keys(x.message)[0] === 'ephemeralMessage') ? x.message.ephemeralMessage.message : x.message
+        if (x.key && x.key.remoteJid === 'status@broadcast') return
+        
+        const m = smsg(inky, x)
+        require('./message/upsert')(inky, m, mek)
+    })
+    
+    inky.ev.on('connection.update', async (update) => {
+        const { connection, lastDisconnect } = update
+        if (connection === 'close') {
+            lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? start() : console.log('Bot Desconectado')
+        }
+    })
+    
+    inky.ev.on('creds.update', saveState)
+    
+    return inky
 }
 
 start()
